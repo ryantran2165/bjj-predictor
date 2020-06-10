d370aa22c21deec697eacb72059d7e6a52809c9d
ryan_tran <ryantran2165@gmail.com>
2020-06-10 16:55:27 -0700

    Updates

---
+++ precache-manifest.ea2f927f124e6dd345135d4c261323ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba26965f8c87ab6c0027ee04531df33",
+    "url": "/bjj-predictor/index.html"
+  },
+  {
+    "revision": "a8f20e233ed151c8d18b",
+    "url": "/bjj-predictor/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1f41531c0323d3b817d3",
+    "url": "/bjj-predictor/static/css/main.5dfaeab6.chunk.css"
+  },
+  {
+    "revision": "a8f20e233ed151c8d18b",
+    "url": "/bjj-predictor/static/js/2.03f34221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bjj-predictor/static/js/2.03f34221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f41531c0323d3b817d3",
+    "url": "/bjj-predictor/static/js/main.3cfe64bc.chunk.js"
+  },
+  {
+    "revision": "58af35bf6069211997cf",
+    "url": "/bjj-predictor/static/js/runtime-main.bcb06902.js"
+  }
+]);
